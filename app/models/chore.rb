128c732e41ec64f283e4b1ddea20db7a0bb95118
ankitaku2019 <ankitaku@andrew.cmu.edu>
2021-02-08 00:26:14 -0600

    Added in all the validations, scopes, and methods for chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,25 @@
 class Chore < ApplicationRecord
+#Relationships
+belongs_to :child
+belongs_to :task
+
+#Validations
+validates_date :due_on
+
+#Scope
+scope :by_task, -> {includes(:task).order('tasks.name')}
+scope :chronological, -> {joins(:task).order ('due_on, name')}
+scope :pending, -> {where(completed:false)}
+scope :done, -> {where(completed:true)}
+scope :upcoming, -> {where("due_on >= ?", Date.current)}
+scope :past, -> {where("due_on < ?", Date.current)}
+
+#Methods
+def status
+    if self.completed
+        return "Completed"
+    else
+        return "Pending"
+    end
+end
 end
