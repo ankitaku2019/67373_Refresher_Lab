f0cd16284b1c0c956890364a91a3f6cb49f45fc4
ankitaku2019 <ankitaku@andrew.cmu.edu>
2021-02-07 23:31:58 -0600

    Added the relationships and validations

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,14 @@
 class Child < ApplicationRecord
+
+#Relationships
+has_many :chores
+has_many :tasks, through: :chores
+
+#Validations
+validates_presence_of :first_name, :last_name
+
+#Scopes
+
+
     
 end
